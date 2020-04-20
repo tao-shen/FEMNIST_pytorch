cc5a5ae2bb7d2f4adb7934b50e58a66452fc8a14
tao-shen <tao.shen@zju.edu.cn>
2020-04-20 20:56:25 +0800

    dataset

--- femnist.py
+++ femnist.py
@@ -1,20 +1,19 @@
 import torch
-# from torch.utils.data import Dataset
-from torchvision.datasets import MNIST
-# from PIL import Image
-import os.path
+from torchvision import transforms, datasets
+from torch.utils.data import Subset, DataLoader
+from PIL import Image
 
 
-class FEMNIST(MNIST):
+class FEMNIST(datasets.MNIST):
+    classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
+               '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine']
 
-    def __init__(self, root, train=True, transform=None, target_transform=None):
-        super(MNIST, self).__init__(root, transform=None, target_transform=None)
-        if train:
-            self.data, self.targets = torch.load('train.pt')
-        else:
-            self.data, self.targets = torch.load('test.pt')
-        self.transform = transform
-        self.target_transform = target_transform
+    def __getitem__(self, index):
 
-    def __len__(self):
-        return len(self.data)
+        img, target = self.data[index], int(self.targets[index])
+        img = Image.fromarray(img.numpy(), mode='F')
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return img, target
